7e8715dd2e89e176361e094d6bf229f88371e570
Nicolas Möhlinger <nicocmoh@gmail.com>
2023-02-22 23:10:01 -0300

    se agrega opcion de reinicio de carrito

--- js/script.js
+++ js/script.js
@@ -1,46 +1,34 @@
-
 let nombre = prompt('Ingrese su nombre');
 let apellido = prompt('Ingrese su apellido');
-if (nombre != '' || apellido != '') {
+if (nombre != '' && apellido != '') {
     alert('Bienvenido/a ' + nombre + ' ' + apellido)
 } else {
     alert('Faltan datos')
 }
-
 let savedContraseña = '3estrellas';
-
 function login() {
     let ingresar = false;
     for (let i = 2; i >= 0; i--) {
-
         let contraseña = prompt('ingrese su contraseña. Tenes ' + (i + 1) + ' oportunidades.');
-
         if (contraseña === savedContraseña) {
-
             alert('Bienvenido a su local de venta de alimento balanceado para mascotas.');
             ingresar = true;
             break;
-
         } else {
             alert('Error. Te quedan ' + i + ' oportunidades.')
         }
-
     }
     return ingresar;
 }
-
 if (login()) {
     let precioBalanceadoPerros = 10000;
     let precioBalanceadoGatos = 8000;
     let precioHumedo = 5000;
-
     const suma = (a, b) => a + b;
     const resta = (a, b) => a - b;
     const descuento = x => x * 0.20
-
-    let opcion = prompt('Elegi una opción: \n 1- Alimento balanceado para perro.\n 2- Alimento balanceado para gato. \n 3- Alimento húmedo.\n 4- Promoción alimento para perro + húmedo. \n 5- Promoción alimento para gato + húmedo. \n 6- Total compra. \n 7- Ingresar NO para salir.')
+    let opcion = prompt('Elegí una opción: \n 1- Alimento balanceado para perros (Precio $10.000.-).\n 2- Alimento balanceado para gatos (Precio $8.000.-). \n 3- Alimento húmedo (Precio $5.000.-).\n 4- Promoción alimento para perro + húmedo (20% de descuento). \n 5- Promoción alimento para gato + húmedo (20% de descuento). \n 6- Total de la compra. \n 7- Reiniciar el carrito de compras. \n 8- Ingresar NO para salir.')
     let total = 0;
-
     while (opcion != 'no' && opcion != 'NO') {        
         switch (opcion) {
             case '1':
@@ -66,17 +54,19 @@ if (login()) {
                 total = total + nuevoPrecio2;
                 break;
             case '6':
-                alert('Su total es: '+ total);
-                break;    
+                alert('Su total es: $ '+ total);
+                break;  
+            case '7':
+                total = 0;
+                alert('Se reinició el carrito de compras.')  
+                break;
             default:
-                alert('Opción invalida.')
+                alert('Opción inválida.')
                 break;
         }
-        opcion = prompt('Elegi una opción: \n 1- Alimento balanceado para perros.\n 2- Alimento balanceado para gatos. \n 3- Alimento húmedo. \n 4- Promoción alimento para perro + húmedo. \n 5- Promoción alimento para gato + húmedo.  \n 6- Total compra. \n 7- Ingresar NO para salir.')
-
+        opcion = prompt('Elegí una opción: \n 1- Alimento balanceado para perros (Precio $10.000.-).\n 2- Alimento balanceado para gatos (Precio $8.000.-). \n 3- Alimento húmedo (Precio $5.000.-).\n 4- Promoción alimento para perro + húmedo (20% de descuento). \n 5- Promoción alimento para gato + húmedo (20% de descuento). \n 6- Total de la compra. \n 7- Reiniciar el carrito de compras. \n 8- Ingresar NO para salir.')
     }
 } else {
     alert('Vuelva a intentar ingresar.')
 }
-
 alert('Gracias por visitarnos.')
